6d1a56e885cf75c600f7e39c23c2555d97490baa
Madeline Charles <mc0622@byu.edu>
2025-11-19 14:54:02 -0700

    11/19/2025

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TRPC_CLIENT } from '../../utils/trpc.client';
+import { trpcResource } from '@fhss-web-team/frontend-utils';
 
 @Component({
   selector: 'app-tasks',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './tasks.page.html',
   styleUrl: './tasks.page.scss',
 })
-export class TasksPage {}
+export class TasksPage {
+  private readonly trpc = inject(TRPC_CLIENT);
+
+  protected readonly PAGE_SIZE = 12 as const;
+  private readonly pageOffset = signal(0);
+
+  protected readonly taskResource = trpcResource(
+    this.trpc.tasks.getTasksByUser.mutate,
+    () => ({
+      pageSize: this.PAGE_SIZE,
+      pageOffset: this.pageOffset(),
+    }),
+    { autoRefresh: true }
+  );
+}
